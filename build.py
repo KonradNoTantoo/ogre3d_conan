b675644e0effa9407cfdd3f0fa0dfada5369249c
Knife <konrad@no.tantoo>
2020-02-22 11:01:59 +0100

    Evil typo is evil

--- build.py
+++ build.py
@@ -2,10 +2,10 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(option=["ogre3d:with_cg=False"])
+    builder = ConanMultiPackager(options=["ogre3d:with_cg=False"])
     builder.add_common_builds()
     builder.run()
 
-    builder = ConanMultiPackager(option=["ogre3d:with_cg=True"])
+    builder = ConanMultiPackager(options=["ogre3d:with_cg=True"])
     builder.add_common_builds()
     builder.run()
