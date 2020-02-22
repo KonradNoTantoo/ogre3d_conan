7a4573f885babf0c268410e2e77e58bba438d57a
Knife <konrad@no.tantoo>
2020-02-22 20:16:16 +0100

    mesa needs a mako upgrade

--- build.py
+++ build.py
@@ -2,10 +2,10 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(options=["ogre3d:with_cg=False"], docker_32_images=True)
+    builder = ConanMultiPackager(options=["ogre3d:with_cg=False"], docker_32_images=True, pip_install=["mako"])
     builder.add_common_builds()
     builder.run()
 
-    builder = ConanMultiPackager(options=["ogre3d:with_cg=True"], docker_32_images=True)
+    builder = ConanMultiPackager(options=["ogre3d:with_cg=True"], docker_32_images=True, pip_install=["mako"])
     builder.add_common_builds()
     builder.run()
