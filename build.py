1ec4862a72f21c04575b83511e4a08c25ea4c1c4
Knife <konrad@no.tantoo>
2020-02-22 19:44:26 +0100

    Don't cross compile on Linux

--- build.py
+++ build.py
@@ -2,10 +2,10 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(options=["ogre3d:with_cg=False"])
+    builder = ConanMultiPackager(options=["ogre3d:with_cg=False"], docker_32_images=True)
     builder.add_common_builds()
     builder.run()
 
-    builder = ConanMultiPackager(options=["ogre3d:with_cg=True"])
+    builder = ConanMultiPackager(options=["ogre3d:with_cg=True"], docker_32_images=True)
     builder.add_common_builds()
     builder.run()
