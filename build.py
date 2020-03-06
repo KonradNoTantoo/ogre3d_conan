e87e4c5f1deaf60117e26b61b6efffa63e64dae7
Knife <konrad@no.tantoo>
2020-03-06 20:05:46 +0100

    Spit builds

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
+import build
+import os
 from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(options=["ogre3d:with_cg=False"], docker_32_images=True, pip_install=["mako"])
-    builder.add_common_builds()
-    builder.run()
+    ci_ogre_architecture = os.environ['CI_OGRE_ARCHITECTURE']
+    ci_ogre_with_cg = os.environ['CI_OGRE_WITH_CG']
 
-    builder = ConanMultiPackager(options=["ogre3d:with_cg=True"], docker_32_images=True, pip_install=["mako"])
-    builder.add_common_builds()
+    builder = ConanMultiPackager(docker_32_images=True, pip_install=["mako"])
+    builder.add(settings={"arch": ci_ogre_architecture, "build_type": "Release"},
+                options={"ogre:with_cg": "ON" == ci_ogre_with_cg})
+    builder.add(settings={"arch": ci_ogre_architecture, "build_type": "Debug"},
+                options={"ogre:with_cg": "ON" == ci_ogre_with_cg})
+
     builder.run()
